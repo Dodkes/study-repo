aadb8ef17388055c23c6844d34c02dd02e741be2
Jozef Zitt <84540150+Dodkes@users.noreply.github.com>
2023-05-12 09:55:37 +0200

    Process guideline

---
+++ Process guideline/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Role from './Role';
+
+function App() {
+  return (
+    <>
+    <h1 className='main-heading'>1TOC Process guideline</h1>
+    <Role roleName={'Network'}/>
+    <Role roleName={'TSY SNOW'}/>
+    <Role roleName={'Queue'}/>
+    </>
+  )
+  
+}
+
+export default App;
---
+++ Process guideline/src/Role.js
@@ -0,0 +1,5 @@
+export default function Role (props) {
+    return (
+            <button className="role-tab">{props.roleName}</button>
+    )
+}
