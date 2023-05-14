9496f9ecc954e7dc33507e67382830fe513c7011
Jozef Zitt <84540150+Dodkes@users.noreply.github.com>
2023-05-14 12:48:46 +0200

    Process guide component & functionality

--- Process guideline/src/App.js
+++ Process guideline/src/App.js
@@ -1,16 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 import Role from './Role';
 
 function App() {
+
+const [data, setData] = useState('')
+
+  const getRoleData = (data) => {
+    switch (data) {
+      case 'network' : setData('Network')
+        break
+      case 'teams' : setData('Teams')
+        break
+        case 'tsysnow' : setData('tsysnow')
+        break
+        case 'other' : setData('other')
+          break
+        default:
+    }
+  }
+
   return (
     <>
     <h1 className='main-heading'>1TOC Process guideline</h1>
-    <Role roleName={'Network'}/>
-    <Role roleName={'TSY SNOW'}/>
-    <Role roleName={'Queue'}/>
+    <Role roleName={'Network'} id={'network'} getRoleData={getRoleData}/>
+    <Role roleName={'TSY SNOW'} id={'tsysnow'} getRoleData={getRoleData}/>
+    <Role roleName={'Teams'} id={'teams'} getRoleData={getRoleData}/>
+    <Role roleName={'Other'} id={'other'} getRoleData={getRoleData}/>
+    <Guide getRoleData={getRoleData} data={data}/>
     </>
   )
-  
+}
+
+function Guide (props) {
+    return <div id='guide-container'>{props.data}</div>
 }
 
 export default App;
--- Process guideline/src/Role.js
+++ Process guideline/src/Role.js
@@ -1,5 +1,16 @@
 export default function Role (props) {
-    return (
-            <button className="role-tab">{props.roleName}</button>
-    )
+   function displayProcesses (e) {
+
+        const roleIDs = ['network', 'tsysnow', 'teams', 'other']
+        roleIDs.forEach(element => {
+                document.getElementById(element).style.backgroundColor = 'transparent'
+                document.getElementById(element).style.color = 'white'
+        });
+        e.target.style.backgroundColor = 'white'
+        e.target.style.color = 'black'
+        props.getRoleData(e.target.id)
+   }
+
+    return <button id={props.id} onClick={displayProcesses} className="role-tab">{props.roleName}</button>
 }
+
